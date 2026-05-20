765c4c3a77d36dffc9a971d993247f4db99c730a
Ron <victaaronniel73@gmail.com>
2026-05-20 18:35:25 +0800

    Revert "Redesign NavBar into a premium floating glassmorphism capsule"
    This reverts commit 35d1ece146d0efe354e9a8c5881504a57bfbf461.

--- contemporary-world/src/components/layout/NavBar/NavBar.jsx
+++ contemporary-world/src/components/layout/NavBar/NavBar.jsx
@@ -1,26 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Logo from "../../common/Logo/Logo";
 import "./NavBar.css";
 
 const NavBar = () => {
   const [activeTab, setActiveTab] = useState("Home");
-  const [scrolled, setScrolled] = useState(false);
   const navItems = ["Home", "Topics", "References", "About Us"];
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 20) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
+    <nav className="navbar">
       <Logo />
       <ul className="nav-links">
         {navItems.map((item) => (
