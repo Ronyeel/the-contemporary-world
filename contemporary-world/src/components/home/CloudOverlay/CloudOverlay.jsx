6fb77c60da71825aea485975c5d285ea8a98c2c1
Ron <victaaronniel73@gmail.com>
2026-05-20 19:07:45 +0800

    Create continuous banner of overlapping cloud assets covering the boundary line fully from left to right

--- contemporary-world/src/components/home/CloudOverlay/CloudOverlay.jsx
+++ contemporary-world/src/components/home/CloudOverlay/CloudOverlay.jsx
@@ -4,23 +4,32 @@ import "./CloudOverlay.css";
 
 const CloudOverlay = () => {
   return (
-    <div className="clouds-container">
-      {/* Cloud Layer 1: Left side fluffy cloud */}
-      <img 
-        src={cloud1} 
-        className="cloud-layer cloud-layer--1" 
-        alt="Transition Cloud Left" 
-      />
-      
-      {/* Cloud Layer 2: Right side volumetric cloud */}
-      <img 
-        src={cloud2} 
-        className="cloud-layer cloud-layer--2" 
-        alt="Transition Cloud Right" 
-      />
-      
-      {/* Blending overlay to merge the bottom edge into the white section */}
-      <div className="cloud-blend-overlay"></div>
+    <div className="clouds-wrapper">
+      <div className="clouds-container">
+        {/* Cloud Layer 1: Left side fluffy cloud */}
+        <img 
+          src={cloud1} 
+          className="cloud-layer cloud-layer--1" 
+          alt="Transition Cloud Left" 
+        />
+        
+        {/* Cloud Layer 2: Center overlapping cloud */}
+        <img 
+          src={cloud2} 
+          className="cloud-layer cloud-layer--center" 
+          alt="Transition Cloud Center" 
+        />
+        
+        {/* Cloud Layer 3: Right side volumetric cloud */}
+        <img 
+          src={cloud1} 
+          className="cloud-layer cloud-layer--2" 
+          alt="Transition Cloud Right" 
+        />
+        
+        {/* Blending overlay to merge the bottom edge into the white section */}
+        <div className="cloud-blend-overlay"></div>
+      </div>
     </div>
   );
 };
